f0167466df878c86910ea654bb33564fd8a193f5
MatteoWebDesigner <matteowebdesigner@googlemail.com>
2018-06-13 13:50:13 +0100

    added some inputs and manifest json

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,30 @@ let template = `
             <mdc-top-app-bar title="Work log" icon=""></mdc-top-app-bar>    
         </mdc-toolbar>
         
-        <main>
-            Hello world
+        <main class="App_main-content">
+            <h2>Add a record</h2>
+            <mdc-textfield v-model="timeStart" label="time start" />
+            <mdc-textfield v-model="timeEnd" label="time end" />
+            <mdc-textfield v-model="label" label="label" />
+            <mdc-button @click="onSubmit" raised>Add log</mdc-button>
         </main>
 
     </mdc-layout-app>
 `
 
 export default Vue.component('App', {
+    data() {
+        return {
+            timeStart: "",
+            timeEnd: "",
+            label: "",
+            date: ""
+        }
+    },
+    methods: {
+        onSubmit() {
+
+        }
+    },
     template
 });
